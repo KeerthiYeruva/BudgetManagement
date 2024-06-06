bef01c933601185b26089b9c7e200ce5bd248324
KeerthiYeruva <keerthisri.yeruva@gmail.com>
2024-06-06 21:58:57 +0200

    megre mui branch

--- src/styles/components/ToggleCustomTheme.tsx
+++ src/styles/components/ToggleCustomTheme.tsx
@@ -1,5 +1,4 @@
 import * as React from "react";
-import Box from "@mui/material/Box";
 import ToggleButton from "@mui/material/ToggleButton";
 import ToggleButtonGroup from "@mui/material/ToggleButtonGroup";
 import AutoAwesomeRoundedIcon from "@mui/icons-material/AutoAwesomeRounded";
@@ -14,35 +13,24 @@ export const ToggleCustomTheme = ({
   toggleCustomTheme,
 }: ToggleCustomThemeProps) => {
   return (
-    <Box
+    <ToggleButtonGroup
+      color="primary"
+      exclusive
+      value={showCustomTheme}
+      onChange={toggleCustomTheme}
+      aria-label="Platform"
       sx={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        width: "100dvw",
-        position: "fixed",
-        bottom: 24,
+        backgroundColor: "background.default",
+        "& .Mui-selected": {
+          pointerEvents: "none",
+        },
       }}
     >
-      <ToggleButtonGroup
-        color="primary"
-        exclusive
-        value={showCustomTheme}
-        onChange={toggleCustomTheme}
-        aria-label="Platform"
-        sx={{
-          backgroundColor: "background.default",
-          "& .Mui-selected": {
-            pointerEvents: "none",
-          },
-        }}
-      >
-        <ToggleButton value>
-          <AutoAwesomeRoundedIcon sx={{ fontSize: "20px", mr: 1 }} />
-          Theme 1
-        </ToggleButton>
-        <ToggleButton value={false}>Theme 2</ToggleButton>
-      </ToggleButtonGroup>
-    </Box>
+      <ToggleButton value>
+        <AutoAwesomeRoundedIcon sx={{ fontSize: "20px", mr: 1 }} />
+        Theme 1
+      </ToggleButton>
+      <ToggleButton value={false}>Theme 2</ToggleButton>
+    </ToggleButtonGroup>
   );
 };
